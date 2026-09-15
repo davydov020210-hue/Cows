6deb5f58673e750055527edaa4c0b927bafbf060
user <your_email@example.com>
2026-09-15 19:25:04 +0700

    first commit

---
+++ cows.py
@@ -0,0 +1,134 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, User
+from aiogram.filters.command import Command
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+from func import *
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token="8769780535:AAGa3m15uSR7IYa_U74OLVUHUNjKZabGUiM")
+dp = Dispatcher()
+
+class Milk(StatesGroup):
+    wait_milk_count_for_sale = State()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    print(message.from_user.id)
+    print(message.chat.id)
+    user_in_base = get_user(message.from_user.id)
+    if not user_in_base:
+        reg(message.from_user.id)
+
+    update(user_id=message.from_user.id)
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Мои коровы", callback_data="my_cows"), InlineKeyboardButton(text="Купить коров", callback_data="buy_cows"), InlineKeyboardButton(text="Продать молоко", callback_data="sell_milk")], [InlineKeyboardButton(text="Обновить", callback_data="back_to_main_menu")]])
+    await message.answer(f"{stat(user_id=message.from_user.id)}", reply_markup=keyboard)
+
+@dp.callback_query(F.data.startswith("my_cows"))
+async def my_cows(callback: types.CallbackQuery):
+    cows = my_cows_func(callback.message.chat.id)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Назад", callback_data="back_to_main_menu")]])
+    await callback.message.edit_text(cows, reply_markup=keyboard)
+
+@dp.callback_query(F.data == "back_to_main_menu")
+async def back_to_main_menu(callback: types.CallbackQuery, state: FSMContext):
+    update(user_id=callback.message.chat.id)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Мои коровы", callback_data="my_cows"), InlineKeyboardButton(text="Купить коров", callback_data="buy_cows"), InlineKeyboardButton(text="Продать молоко", callback_data="sell_milk")], [InlineKeyboardButton(text="Обновить", callback_data="back_to_main_menu")]])
+    await callback.message.edit_text(f"{stat(user_id=callback.message.chat.id)}", reply_markup=keyboard)
+    await state.clear()
+
+async def periodic_task():
+    while True:
+        now = time.time()
+
+        seconds_passed_this_hour = now % 3600
+        seconds_to_wait = 3600 - seconds_passed_this_hour
+
+        await asyncio.sleep(seconds_to_wait)
+
+        change_milk_price()
+
+@dp.callback_query(F.data == "sell_milk")
+async def sell_milk_amount(callback: types.CallbackQuery, state: FSMContext):
+    milk = get_user(callback.message.chat.id)[1]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="25%", callback_data="sell_25"), InlineKeyboardButton(text="50%", callback_data="sell_50"), InlineKeyboardButton(text="75%", callback_data="sell_75"), InlineKeyboardButton(text="100%", callback_data="sell_100")], [InlineKeyboardButton(text="Назад", callback_data="back_to_main_menu")]])
+    await callback.message.edit_text( f"У вас молока: {milk}\n" f"Молоко стоит: {get_milk_price()} руб\n" f"Сколько молока продать?", reply_markup=keyboard )
+    await state.set_state(Milk.wait_milk_count_for_sale)
+
+@dp.callback_query(F.data.startswith("sell_"))
+async def sell_milk_percent(callback: types.CallbackQuery):
+    percent = int(callback.data.replace("sell_", ""))
+
+    milk = get_user(callback.from_user.id)[1]
+
+    amount = int(milk * percent / 100)
+
+    result = sell_milk_func(user_id=callback.from_user.id, user_milk_sell=amount)
+
+    await callback.message.edit_text(f"Продано молока: {amount}\n Получено: {amount * MILK_PRICE} руб\n {result}", reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Продать ещё",callback_data="sell_milk")],[InlineKeyboardButton(text="Назад",callback_data="back_to_main_menu")]]))
+
+#@dp.callback_query(F.data == "update")
+#async def update
+
+@dp.callback_query(F.data.startswith("buy_cows"))
+async def choose_cow(callback: types.CallbackQuery):
+
+    choose_keyboard = InlineKeyboardMarkup(inline_keyboard=cow_keyboard())
+    await callback.message.edit_text("Выберите корову:", reply_markup=choose_keyboard)
+
+def cow_keyboard():
+    buttons = []
+    
+    for cow in get_cows():
+        buttons.append([InlineKeyboardButton(text=f"{cow[1]}, {cow[3]}руб.", callback_data=f"buy-ask_{cow[0]}")])
+    buttons.append([InlineKeyboardButton(text="Главное меню", callback_data="back_to_main_menu")])
+    return buttons
+
+@dp.callback_query(F.data.startswith("buy-ask"))
+async def buy_normal__cow(callback: types.CallbackQuery):
+    cow_id = callback.data.replace("buy-ask_", "")
+    cow = get_cow(cow_id)
+    if cow:
+        buy_keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Да", callback_data=f"buy_{cow_id}"), InlineKeyboardButton(text="Нет", callback_data="no_buy")]])
+        await callback.message.edit_text(f"Цена коровы: {cow[3]}", reply_markup=buy_keyboard)
+    else:
+        await callback.answer("Корова не найдена")
+
+@dp.callback_query(F.data.startswith("buy_"))
+async def buy_cow(callback: types.CallbackQuery):
+    cow_id = callback.data.replace("buy_", "")
+
+    result = buy_cow_func(user_id=callback.from_user.id, cow_id=cow_id)
+
+    if result:
+        await callback.answer("Корова приобретена")
+        await choose_cow(callback)
+    else:
+        await callback.answer("Ошибка при покупке")
+        await choose_cow(callback)
+
+@dp.callback_query(F.data.startswith("reg"))
+async def register(callback: types.CallbackQuery):
+    reg(callback.from_user.id)
+    await callback.message.answer("Пользователь добвлен")
+    await callback.message.answer(text=my_cows_func(callback.from_user.id))
+
+@dp.message(Milk.wait_milk_count_for_sale)
+async def sell_milk(message: types.Message):
+    try:
+        amount = int(message.text)
+    except ValueError:
+        await message.answer("Введите число")
+        return
+    result = sell_milk_func(user_id=message.from_user.id, user_milk_sell=amount)
+    await message.answer(result)
+
+async def main():
+    asyncio.create_task(periodic_task())
+    await dp.start_polling(bot)
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ func.py
@@ -0,0 +1,168 @@
+import sqlite3, random
+import time
+con = sqlite3.connect("cows.db")
+cur = con.cursor()
+MILK_PRICE = 100
+
+def reg(user_id):
+    cows = get_cows()
+    sql = "INSERT INTO Пользователи(id, Деньги, Молоко, Последние_обновления, "
+    for cow in cows:
+        sql += f"{cow[4]}, "
+    sql = sql[0:-2]
+    sql +=f" ) VALUES(?, 5000, 0, {time.time()}, " + "0, "*len(cows)
+    sql = sql[0:-2]
+    sql += ")"
+    cur.execute(sql, (user_id,))
+    con.commit()
+
+def get_user(user_id):
+    cur.execute("SELECT * FROM Пользователи WHERE id = ?", (user_id,))
+    return cur.fetchone()
+
+def add_milk(user_id, count):
+    cur.execute("SELECT Молоко FROM Пользователи WHERE id = ?", (user_id,))
+    milk = cur.fetchone()[0]
+    cur.execute("UPDATE Пользователи SET Молоко = ? WHERE id = ?", (milk + count, user_id,))
+    con.commit()
+
+def update(user_id):
+    cows = get_cows()
+    sql = "SELECT "
+    for cow in cows:
+        sql += f"{cow[4]}, "
+    sql = sql[0:-2]
+    sql += " FROM Пользователи WHERE id = ?"
+    print(sql)
+    print(cows)
+    cur.execute(sql, (user_id,))
+    res = cur.fetchone()
+
+    total = 0
+    for i in range(len(cows)):
+        total += res[i] * cows[i][2]
+
+    cur.execute("SELECT Последние_обновления FROM Пользователи WHERE id = ?", (user_id,))
+    old_time = cur.fetchone()[0]
+    new_time = time.time()
+    delta_time = new_time - old_time
+    days = delta_time / 86400
+    add_milk(user_id, total*days)
+    cur.execute("UPDATE Пользователи SET Последние_обновления = ? WHERE id = ?", (new_time, user_id,))
+
+def change_milk_price():
+    global MILK_PRICE
+
+    old_price = MILK_PRICE
+    MILK_PRICE = random.randint(50, 150)
+
+    print(f"Цена молока изменилась: {old_price} - {MILK_PRICE}")
+
+    return MILK_PRICE
+
+#async def main():
+#    task = asyncio.create_task(periodic_task())
+#    await task
+
+#if __name__ == "__main__":
+#    asyncio.run(main())
+
+def stat(user_id):
+    cows = get_cows()
+    sql = "SELECT Молоко, Деньги, "
+    for cow in cows:
+        sql += f"{cow[4]}, "
+    sql = sql[0:-2]
+    sql += " FROM Пользователи WHERE id = ?"
+    print(sql)
+    cur.execute(sql, (user_id,))
+    res = cur.fetchone()
+    print(res)
+
+    milk, money = res[0:2]
+    total_cows = 0
+    for count in res[2:]:
+        total_cows += count
+    
+    return(f"Деньги: {money}. Молоко: {milk}. Всего коров: {total_cows}")
+
+def get_cow(cow_id):
+    cur.execute("SELECT * FROM Коровы WHERE id = ?", (cow_id,))
+    result = cur.fetchone()
+
+    return result
+
+def get_cows():
+    cur.execute("SELECT * FROM Коровы")
+    result = cur.fetchall()
+    
+    return result
+
+def get_milk_price():
+    return MILK_PRICE
+
+def get_cow_price(cow_id):
+    cur.execute("SELECT Цена FROM Коровы WHERE id = ?", (cow_id,))
+    result = cur.fetchone()
+    if result is None:
+        return None
+    return result[0]
+
+def sell_milk_func(user_id, user_milk_sell):
+    cur.execute("SELECT Молоко, Деньги FROM Пользователи WHERE id = ?",(user_id,))
+    result = cur.fetchone()
+
+    if result is None:
+        return "Пользователь не найден"
+
+    user_milk, user_money = result
+
+    user_milk_sell = int(user_milk_sell)
+
+    if user_milk_sell <= 0:
+        return "Количество молока должно быть больше нуля"
+
+    ost = user_milk - user_milk_sell
+
+    if ost < 0:
+        return "Недостаточно молока"
+
+    earned_money = user_milk_sell * get_milk_price()
+    new_money = user_money + earned_money
+
+    cur.execute("UPDATE Пользователи SET Молоко = ?, Деньги = ? WHERE id = ?",(ost, new_money, user_id))
+    con.commit()
+
+    return f"Получено: {earned_money} денег"
+
+def buy_cow_func(user_id, cow_id):
+    update(user_id)
+    cur.execute("SELECT Деньги FROM Пользователи WHERE id = ?", (user_id,))
+    user_balance = cur.fetchone()[0]
+    cow = get_cow(cow_id)
+    ost = user_balance-cow[3]
+    print(ost, user_balance, cow[3])
+    if ost >= 0:
+        cur.execute("UPDATE Пользователи SET Деньги = ? WHERE id = ?", (ost, user_id,))
+        cur.execute(f"UPDATE Пользователи SET {cow[4]} = {cow[4]} + 1 WHERE id = ?", (user_id,))
+        con.commit()
+
+        return True
+    else:
+        return False
+    
+
+def my_cows_func(user_id):
+    cows = get_cows()
+    sql = "SELECT "
+    for cow in cows:
+        sql += f"{cow[4]}, "
+    sql = sql[0:-2]
+    sql += " FROM Пользователи WHERE id = ?"
+    cur.execute(sql, (user_id,))
+    user_cows = cur.fetchone()
+
+    text = ""
+    for i in range(len(cows)):
+        text += f"{cows[i][1]}: {user_cows[i]}\n"
+    return text
